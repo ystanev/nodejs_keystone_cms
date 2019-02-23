2016e5a9965d025e5ff1e336a3829e4cbf623552
Yury Stanev <ystanev@yahoo.ca>
2019-02-23 14:26:10 -0500

    Initial Commit

---
+++ .history/models/Product_20190223122336.js
@@ -0,0 +1,19 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+var Product = new keystone.List('Product', {
+	map: {name: 'title'},
+	singular: 'Product',
+	plural: 'Products',
+	autokey: {path: 'slug', from: 'title', unique: true}
+});
+
+Product.add({
+	title: {type: String, required: true},
+	price:{type: Number},
+	qty: {type: Number},
+	description: {type: Types.Html, wysiwyg: true, height: 300},
+	image: {type: Date, default: Date.now}
+});
+
+Product.register();
